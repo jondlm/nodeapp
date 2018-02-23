42ca93009d26c5fcd32b7d3909b590336f129a5b
Jon de la Motte <jdelamotte@appnexus.com>
2018-02-23 14:19:40 -0800

    Add env printing

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello from nodeapp!'))
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+app.listen(80, () => {
+  console.log('Example app listening on port 80!')
+  for (var i in process.env) {
+    console.log(`${i}=${process.env[i]}`)
+  }
+})
