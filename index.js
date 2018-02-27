75cd0684c3818004d06b70ece12e8d0082394603
Jon de la Motte <jdelamotte@appnexus.com>
2018-02-27 08:21:24 -0800

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => res.send('Hello from nodeapp!'))
 
 app.listen(80, () => {
   console.log('Example app listening on port 80!')
+  console.log('Env vars:')
   for (var i in process.env) {
     console.log(`${i}=${process.env[i]}`)
   }
